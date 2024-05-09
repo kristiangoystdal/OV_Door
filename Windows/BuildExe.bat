298a61ef057f44d65294afb36f1f60da2b51508f
Kristian Goystdal <krisgoy100@gmail.com>
2024-05-09 13:31:28 +0200

    chore: Switch from cx_freeze to pyinstaller

--- Windows/BuildExe.bat
+++ Windows/BuildExe.bat
@@ -1,26 +1,21 @@
 @echo off
-echo Building the Omega Verksted executable using cx_Freeze...
-python setup.py build
-echo Build process complete.
 
-:: Create the dist directory if it does not exist
-if not exist "dist\" mkdir "dist"
+rem Set the path to the Python interpreter
+set "PYTHON_EXECUTABLE=python.exe"
 
-:: Define the source directory and file name based on your actual build output
-:: This needs to be adjusted according to your actual output path and Python environment
-set "source_dir=build\exe.win-amd64-3.11"
-set "executable_name=ov_door.exe"
+rem Set the path to the icon file
+set "ICON_FILE=C:\Users\krisg\Documents\Git\OV_Door\Windows\ov_logo.ico"
 
-:: Check if the executable exists and move it if it does
-if exist "%source_dir%\%executable_name%" (
-    move "%source_dir%\%executable_name%" "dist\Omega Verksted.exe"
-    if "%ERRORLEVEL%" == "0" (
-        echo Executable moved and renamed successfully.
-    ) else (
-        echo Failed to move executable.
-    )
-) else (
-    echo Error: Executable not found in the expected directory.
-)
+rem Remove the 'dist' directory if it exists
+if exist dist rmdir /s /q dist
+
+rem Build the executable using PyInstaller
+echo Building the Omega Verksted executable using PyInstaller...
+"%PYTHON_EXECUTABLE%" -m PyInstaller ^
+    --onefile ^
+    --noconsole ^
+    --name "Omega Verksted" ^
+    --icon "%ICON_FILE%" ^
+    ov_door.py
 
 pause
