edced30efb54148485fa08af658bda366ac137e1
Kristian Goystdal <krisgoy100@gmail.com>
2024-05-10 11:24:49 +0200

    chore: Update PyInstaller build process and version handling

--- Windows/BuildExe.bat
+++ Windows/BuildExe.bat
@@ -6,5 +6,9 @@ echo Building the executable with PyInstaller...
 pyinstaller --clean --noupx --windowed --icon="C:\Users\krisg\Documents\Git\OV_Door\Windows\ov_logo.ico" --name="Omega Verksted" ov_door.py
 
 echo Build process complete.
-pause
 
+echo Compressing the dist folder...
+PowerShell -Command "Compress-Archive -Path '.\dist\Omega Verksted\*' -DestinationPath '.\Omega_Verksted.zip'"
+
+echo Compression complete.
+pause
