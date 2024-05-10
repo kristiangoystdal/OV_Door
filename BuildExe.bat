1d11da024198e207627f06c92c22d62e0e6d827c
Kristian Goystdal <krisgoy100@gmail.com>
2024-05-11 01:00:51 +0200

    Add build and zip file to repo

--- BuildExe.bat
+++ BuildExe.bat
@@ -3,12 +3,25 @@ echo Setting up environment...
 set PATH=%PATH%;C:\Users\krisg\AppData\Local\Packages\PythonSoftwareFoundation.Python.3.12_qbz5n2kfra8p0\LocalCache\local-packages\Python312\Scripts
 
 echo Building the executable with PyInstaller...
-pyinstaller --clean --noupx --windowed --icon="C:\Users\krisg\Documents\Git\OV_Door\Windows\ov_logo.ico" --name="Omega Verksted" main.py
+pyinstaller --clean --noupx --windowed --icon="C:\Users\krisg\Documents\Git\OV_Door\ov_logo.ico" --name="Omega Verksted" main.py
 
 echo Build process complete.
 
-echo Compressing the dist folder...
+echo Checking if dist folder exists...
+set RETRIES=0
+:compress
 PowerShell -Command "Compress-Archive -Path '.\dist\Omega Verksted\*' -DestinationPath '.\dist\Omega_Verksted.zip'"
+if %ERRORLEVEL% neq 0 (
+    set /a RETRIES+=1
+    if !RETRIES! lss 5 (
+        echo Attempt !RETRIES! failed, retrying in 5 seconds...
+        timeout /t 5 /nobreak
+        goto compress
+    ) else (
+        echo Failed to compress after multiple attempts.
+    )
+) else (
+    echo Compression complete.
+)
 
-echo Compression complete.
 pause
