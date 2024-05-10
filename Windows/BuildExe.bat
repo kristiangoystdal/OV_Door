14da58ade3b7519a363cf77d71e7825f76737d77
Kristian Goystdal <krisgoy100@gmail.com>
2024-05-11 00:31:34 +0200

    Update executable name in BuildExe.bat

--- Windows/BuildExe.bat
+++ Windows/BuildExe.bat
@@ -3,7 +3,7 @@ echo Setting up environment...
 set PATH=%PATH%;C:\Users\krisg\AppData\Local\Packages\PythonSoftwareFoundation.Python.3.12_qbz5n2kfra8p0\LocalCache\local-packages\Python312\Scripts
 
 echo Building the executable with PyInstaller...
-pyinstaller --clean --noupx --windowed --icon="C:\Users\krisg\Documents\Git\OV_Door\Windows\ov_logo.ico" --name="Omega Verksted" ov_door.py
+pyinstaller --clean --noupx --windowed --icon="C:\Users\krisg\Documents\Git\OV_Door\Windows\ov_logo.ico" --name="Omega Verksted" main.py
 
 echo Build process complete.
 
