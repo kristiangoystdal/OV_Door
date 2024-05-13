d53ccfd2d8182bbb664518f70ff7c17e3ad7a18a
Kristian Goystdal <krisgoy100@gmail.com>
2024-05-13 17:01:32 +0200

    Update installer to use updated download URL and installation path

--- installer.py
+++ installer.py
@@ -179,8 +179,8 @@ def run_installer():
             threading.Thread(
                 target=download_and_extract,
                 args=(
-                    "https://github.com/kristiangoystdal/OV_Door/raw/main/dist/Omega_Verksted.zip",
-                    os.path.join(os.environ["PROGRAMFILES"], "Omega_Verksted"),
+                    "https://github.com/kristiangoystdal/OV_Door/raw/updates/dist/Omega_Verksted.zip",
+                    os.path.join(os.environ["PROGRAMFILES"], "Omega Verksted"),
                     progress_bar,
                     status_label,
                     install_button,
